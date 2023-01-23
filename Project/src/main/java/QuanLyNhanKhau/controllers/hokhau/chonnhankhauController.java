63a3f5e1af0da2a9bb6df5bcbef8f3a3890ec0a1
nthoailinh <linh.nth205231@sis.hust.edu.vn>
2023-01-23 19:17:16 +0700

    Add fxml for confirm information and select a 'nhankhau'

--- Project/src/main/java/QuanLyNhanKhau/controllers/hokhau/chonnhankhauController.java
+++ Project/src/main/java/QuanLyNhanKhau/controllers/hokhau/chonnhankhauController.java
@@ -1,4 +1,18 @@
 package QuanLyNhanKhau.controllers.hokhau;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class chonnhankhauController {
+    @FXML
+    private Button btnTim;
+
+    @FXML
+    private TextField cccd;
+
+    @FXML
+    void handleClicks(ActionEvent event) {
+
+    }
 }
