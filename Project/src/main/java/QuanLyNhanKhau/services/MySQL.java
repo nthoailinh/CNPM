2adbc07061d8c33a2d1bae3d602d7f63dc1212c8
Phan Minh Anh Tuấn <tuanphanminhanh@gmail.com>
2023-02-06 04:05:37 +0000

    Update MySQL.java

--- Project/src/main/java/QuanLyNhanKhau/services/MySQL.java
+++ Project/src/main/java/QuanLyNhanKhau/services/MySQL.java
@@ -6,7 +6,7 @@ public class MySQL {
     public static Connection getConnection() {
         try {
             return DriverManager.getConnection("jdbc:mysql://localhost:3306/quan_ly_nhan_khau_1",
-                    "root", "");
+                    "root", "abc123");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
